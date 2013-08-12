5d0a63468e11edd1fb6fc7f98a2f8d16f5ae9259
Alvaro Peredo <jperedoapb@yahoo.com>
2013-08-11 23:24:25 -0400

    Subiendo la estructura

---
+++ src/org/apb/Sumar.java
@@ -0,0 +1,23 @@
+package org.apb;
+
+public class Sumar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		int a,b,c;
+		a = 10;
+		b = 12;
+		c = ++b + a++;
+		System.out.println("La suma es "+c);
+		System.out.println("El valor de a es "+ a);
+		System.out.println("El valor de b es "+ b);
+		*/
+		System.out.println("Hola mundo");
+
+	}
+
+}
