943aacb77afa79b2d8e46fc888571a0c42de45f3
Alvaro Peredo <jperedoapb@yahoo.com>
2013-08-11 23:26:09 -0400

    Operaciones basicas suma

--- src/org/apb/Sumar.java
+++ src/org/apb/Sumar.java
@@ -7,15 +7,15 @@ public class Sumar {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*
+		
 		int a,b,c;
 		a = 10;
 		b = 12;
-		c = ++b + a++;
+		c = b + a;
 		System.out.println("La suma es "+c);
-		System.out.println("El valor de a es "+ a);
-		System.out.println("El valor de b es "+ b);
-		*/
+		System.out.println("El valor de a es "+ a++);
+		System.out.println("El valor de b es "+ ++b);
+		
 		System.out.println("Hola mundo");
 
 	}
